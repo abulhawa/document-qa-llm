c24b3949f6b2f53dd7acd5d72f771ac33a9f6c36
Ali <17217248+abulhawa@users.noreply.github.com>
2025-08-25 12:07:52 +0200

    Respect CELERY_TASK_QUEUE env for Celery apps

--- worker/celery_worker.py
+++ worker/celery_worker.py
@@ -4,7 +4,9 @@ import os
 # Connect to Redis (host will be the docker-compose service name)
 broker_url = os.getenv("CELERY_BROKER_URL", "redis://localhost:6379/0")
 backend_url = os.getenv("CELERY_RESULT_BACKEND", "redis://localhost:6379/1")
+queue = os.getenv("CELERY_TASK_QUEUE", "celery")
 app = Celery("document_qa_worker", broker=broker_url, backend=backend_url)
+app.conf.task_default_queue = queue
 app.config_from_object("config")
 
 app.autodiscover_tasks(["core"])
